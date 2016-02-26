b9de8f11e289afa9799e562a1ff96e95a879091a
bassomatteo <bassomatteo@Lab06_14.itseinaudi.local>
2016-02-26 12:27:03 +0100

    image

--- src/slot/main.java
+++ src/slot/main.java
@@ -2,8 +2,17 @@ package slot;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Canvas;
+
+import java.util.ArrayList;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
 
 public class main {
+	
+	//public ArrayList<> img = new ArrayList <>(); 
 
 	protected Shell shell;
 
@@ -42,7 +51,10 @@ public class main {
 		shell = new Shell();
 		shell.setSize(1000, 1000);
 		shell.setText("SWT Application");
+		
+		Button btnGira = new Button(shell, SWT.NONE);
+		btnGira.setBounds(899, 10, 75, 25);
+		btnGira.setText("GIRA!");
 
 	}
-
 }
